b0ef47f1c5bba506bd77d69e34af0a3ba4f71640
nikoloz-code23 <73937823+nikoloz-code23@users.noreply.github.com>
2026-07-18 18:53:31 +0400

    Another attempt at deployment configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      // Maps '@' directly to your local 'src' directory
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  plugins: [
-    tailwindcss(),
-  ],
-})
+	base: "/book-database-renderer",
+	resolve: {
+		alias: {
+			// Maps '@' directly to your local 'src' directory
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
+	plugins: [tailwindcss()],
+});
